677432eddacf9817c47203bd421bbfc435b47294
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-19 21:55:10 +0200

    Adding number of unread tweets to emacs mod

--- init.el
+++ init.el
@@ -2,3 +2,6 @@
 
 ;; setting master password to store the password
 (setq twittering-use-master-password t)
+
+;; enabling unread tweets inside the emacs mod
+(twittering-enable-unread-status-notifier)
