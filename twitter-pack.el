8ddf3465ebbf6ee15c829faf2e1484b9e0c40783
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-31 17:41:52 +0200

    Refactor - use package + minor mode twitter-pack

--- twitter-pack.el
+++ twitter-pack.el
@@ -4,24 +4,37 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(twittering-mode))
+(use-package twittering-mode
+  :config
+  ;; enabling unread tweets inside the emacs mod
+  (twittering-enable-unread-status-notifier)
+  ;; setting master password to store the password
+  (custom-set-variables '(twittering-use-master-password t)
+                        ;; display remaining api calls
+                        (twittering-display-remaining t)
+                        ;; inherit hashtag in a RT
+                        (twittering-edit-skeleton 'inherit-any)))
 
-(require 'twittering-mode)
+(defvar twitter-pack-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c t t") 'twittering-mode)
+    map)
+  "Keymap for twitter-pack mode.")
 
-;; setting master password to store the password
-(setq twittering-use-master-password t)
+(define-minor-mode twitter-pack-mode
+  "Minor mode to consolidate twitter-pack extensions.
 
-;; enabling unread tweets inside the emacs mod
-(twittering-enable-unread-status-notifier)
+\\{twitter-pack-mode-map}"
+  :lighter " TP"
+  :keymap twitter-pack-mode-map)
 
-;; display remaining api calls
-(setq twittering-display-remaining t)
+(define-globalized-minor-mode global-twitter-pack-mode twitter-pack-mode twitter-pack-on)
 
-;; inherit hashtag in a RT
-(setq twittering-edit-skeleton 'inherit-any)
+(defun twitter-pack-on ()
+  "Turn on `twitter-pack-mode'."
+  (twitter-pack-mode +1))
 
-(global-set-key (kbd "C-c t t") 'twittering-mode)
+(global-twitter-pack-mode)
 
 (provide 'twitter-pack)
 ;;; twitter-pack ends here
